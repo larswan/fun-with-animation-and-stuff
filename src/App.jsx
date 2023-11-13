8724d4873a67ea5498eddd669f12ed6c8c39dd75
Larson Collier <collierlarson@gmail.com>
2023-11-13 17:11:01 -0500

    fixed y color, made angle gradually change

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
+import GradientMouseTracker from './GradientMouseTracker';
 
 function App() {
-  const [angle, setAngle] = useState(33)
-  const [color1, setColor1] = useState("000000")
-  const [color2, setColor2] = useState("ffffff")
-
-  const gradient= {
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundImage: `linear-gradient(${angle}deg, #${color1}, #${color2})`
-  }
+  const [mouseX, setMouseX] = useState(0)
+  const [mouseY, setMouseY] = useState(0)
 
   const handleMouseMove = (e) => {
-    const mouseX = e.nativeEvent.offsetX;
-    const mouseY = e.nativeEvent.offsetY;
-    console.log(`Mouse Position: X - ${mouseX}, Y - ${mouseY}`)
-    setColor1(`${mouseX}`)
-    setColor2(`${mouseY}`)
-
+    setMouseX(e.nativeEvent.offsetX);
+    setMouseY(e.nativeEvent.offsetY);
   }
-
+  
   return (
-    <div className="gradient" style={gradient} onMouseMove={handleMouseMove}>
-      <h1>#{color1}</h1>
-      <h1>#{color2}</h1>
+    <div className='window' onMouseMove={handleMouseMove} >
+      <GradientMouseTracker mouseX={mouseX} mouseY={mouseY}/>
     </div>
   );
 }
---
+++ src/GradientMouseTracker.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react"
+
+function GradientMouseTracker({mouseX, mouseY}) {
+    const [angle, setAngle] = useState(90)
+    const [xCount, setXCount] = useState(0)
+    const [yCount, setYCount] = useState(0)
+    const [xColor, setXColor] = useState(1749)
+    const [yColor, setYColor] = useState(0)
+
+    // Only change every 10 pixels so it isn't so jarring
+    useEffect(()=>{
+        setAngle((mouseX * mouseY)/10000)
+
+        if(xCount<10){
+            setXCount((prev)=> prev + 1)
+        }
+        else{
+            setXColor(mouseX);
+            setXCount(0)
+        }
+    },[mouseX])
+    
+    useEffect(()=>{
+        setAngle((mouseX * mouseY)/10000)
+        
+        if(yCount<10){
+            setYCount((prev)=> prev + 1)
+        }
+        else{
+            // setYColor(mouseY); 
+            setYCount(0)
+        }
+    },[mouseY])
+
+    const gradient = {
+        width: '20vw',
+        height: '20vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundImage: `linear-gradient(${angle}deg, #${xColor}, #FFFFFF)`
+    }
+
+    return (
+        <div className="gradient" style={gradient} >
+            <h1>#{xColor}</h1>
+            <h1>#{yColor}</h1>
+            <h1>{Math.floor(angle)}°</h1>
+        </div>
+    );
+}
+export default GradientMouseTracker
