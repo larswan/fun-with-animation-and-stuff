957567bcd5276c7712f64a1f6c0d309daec60312
Larson Collier <collierlarson@gmail.com>
2023-11-16 02:10:04 -0500

    gradient colors now determined by a chatGPT algo

--- src/GradientMouseTracker.jsx
+++ src/GradientMouseTracker.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from "react"
+import numberToHexColor from './numberToHexColor'
 
 function GradientMouseTracker({mouseX, mouseY}) {
-    const [angle, setAngle] = useState(90)
+    const [angle, setAngle] = useState(34)
     const [xCount, setXCount] = useState(0)
     const [yCount, setYCount] = useState(0)
     const [xColor, setXColor] = useState(1749)
-    // const [yColor, setYColor] = useState(0)
+    const [yColor, setYColor] = useState(111111)
 
     // Only change every 10 pixels so it isn't so jarring
     useEffect(()=>{
-        setAngle((mouseX * mouseY)/10000)
-        setXColor(mouseX);
+        // setAngle((mouseX * mouseY)/10000)
+        setAngle(mouseY);
+        setXColor(numberToHexColor(mouseX));
 
         // to do fewer rerenders
         // if(xCount<10){
@@ -22,33 +24,24 @@ function GradientMouseTracker({mouseX, mouseY}) {
     },[mouseX])
     
     useEffect(()=>{
-        setAngle((mouseX * mouseY)/10000)
-        
-        if(yCount<10){
-            setYCount((prev)=> prev + 1)
-        }
-        else{
-            // setYColor(mouseY); 
-            setYCount(0)
-        }
+        setYColor(numberToHexColor(mouseY))        
+        // if(yCount<10){
+        //     setYCount((prev)=> prev + 1)
+        // }
+        // else{
+        //     setYColor(mouseY); 
+        //     setYCount(0)
+        // }
     },[mouseY])
 
     const gradient = {
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'start',
-        justifyContent: 'end',
-        borderRadius: '8px',
-        paddingLeft: '1em',
-        backgroundImage: `linear-gradient(${angle}deg, #${xColor}, #FFFFFF)`
+        backgroundImage: `linear-gradient(${angle}deg, ${xColor}, ${yColor})`
     }
 
     return (
-        <div className="gradient" style={gradient} >
-            <h1>#{xColor}</h1>
-            {/* <h1>#{yColor}</h1> */}
+        <div className="gradientBox" style={gradient} >
+            <h1>{xColor}</h1>
+            <h1>{yColor}</h1>
             <h1>{Math.floor(angle)}°</h1>
         </div>
     );
